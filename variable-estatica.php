3487b1134d685f1384b04997fb7825541eebbffc
Ramiro Melero <ramiromelero@gmail.com>
2021-07-08 20:07:59 -0300

    Update variable-estatica.php

--- variable-estatica.php
+++ variable-estatica.php
@@ -1,1 +1,15 @@
+<?php
 
+  function incrementaVariable () {
+     static $contador=0; //variable estática
+    $contador++;
+    
+    echo $contador . "<br>";
+  }
+  
+  incrementaVariable(); //devuelve 1
+  incrementaVariable(); //devuelve 2
+  incrementaVariable(); //devuelve 3
+  incrementaVariable(); //devuelve 4
+    
+?>
