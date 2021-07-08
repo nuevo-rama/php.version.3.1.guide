40e4e7b26782548b396e434b8e2609e0f964344f
Ramiro Melero <ramiromelero@gmail.com>
2021-07-08 20:58:57 -0300

    Update strings.php

--- strings.php
+++ strings.php
@@ -1,1 +1,9 @@
+<?php
+  
+  echo "<p class=\"resaltar\">Párrafo</p>"; //ejemplo 1
+  echo "<p class='resaltar'>Párrafo</p>";  //ejemplo 2
 
+  $nombre="José";
+  echo "Hola $nombre"; //devuelve: Hola José
+  
+?>
