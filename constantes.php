a8f0514f3feb788d43187716396be52abf42d8f6
Ramiro Melero <ramiromelero@gmail.com>
2021-10-12 16:51:45 -0300

    Update constantes.php

--- constantes.php
+++ constantes.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="loginStyles.css">
+</head>
+<body>
 
+<?php
+    #Definir una constante
+    define ("AUTOR", "NR");
+    
+    echo AUTOR;
+
+    echo "El nombre de autor es: " . AUTOR; # El "." está concatenando
+
+?>
+
+</body>
+</html>
