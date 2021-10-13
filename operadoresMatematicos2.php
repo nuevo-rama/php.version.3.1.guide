42ac2ef2f13d9d40302c491f7aa448384be80518
Ramiro Melero <ramiromelero@gmail.com>
2021-10-13 15:20:45 -0300

    Update operadoresMatematicos2.php

--- operadoresMatematicos2.php
+++ operadoresMatematicos2.php
@@ -1,1 +1,37 @@
+<?php
 
+    if(isset ($_POST["button"])) {
+        $primerNumero= $_POST["num1"];
+        $segundoNumero= $_POST["num2"];
+        $operacion= $_POST["operacion"];
+
+        calculos($operacion, $primerNumero, $segundoNumero);
+    }    
+
+    
+
+    function calculos($operacion, $primerNumero, $segundoNumero){
+
+        if(!strcmp("Suma", $operacion)) {
+            $resultado = $primerNumero + $segundoNumero;
+            echo "Resultado: " . $resultado;
+        }
+        if(!strcmp("Resta", $operacion)) {
+            $resultado = $primerNumero - $segundoNumero;
+            echo "Resultado: " . $resultado;
+        }
+        if(!strcmp("Multiplicación", $operacion)) {
+            $resultado = $primerNumero * $segundoNumero;
+            echo "Resultado: " . $resultado;
+        }
+        if(!strcmp("División", $operacion)) {
+            $resultado = $primerNumero / $segundoNumero;
+            echo "Resultado: " . $resultado;
+        }
+        if(!strcmp("Módulo", $operacion)) {
+            $resultado = $primerNumero % $segundoNumero;
+            echo "Resultado: " . $resultado;
+        }
+    }
+    
+?>
