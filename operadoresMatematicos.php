59c50c1f28f2440e6a78c64c0bfb94e4537d59a8
Ramiro Melero <ramiromelero@gmail.com>
2021-10-15 10:58:45 -0300

    Update operadoresMatematicos.php

--- operadoresMatematicos.php
+++ operadoresMatematicos.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
-    <link rel="stylesheet" href="loginStyles.css">
+    <title>Form</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
   
