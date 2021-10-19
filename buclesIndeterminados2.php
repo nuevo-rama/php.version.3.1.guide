a977cd2ad6ab26e045b28f777a3414d0fd709d3c
Ramiro Melero <ramiromelero@gmail.com>
2021-10-19 10:17:27 -0300

    Update buclesIndeterminados2.php

--- buclesIndeterminados2.php
+++ buclesIndeterminados2.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  
+<?php
 
+    $contador=6;
+
+    do {
+
+        echo "Entrada número: $contador"."<br>";
+        $contador++;
+
+    } while ($contador<5);
+
+    echo "Fin del bucle";
+
+?>
+
+</body>
+</html>
